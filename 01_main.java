f2c0b5e0e587b8421e10d8131b4704b6f3cac540
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-26 19:32:44 +0530

    Create 01_main.java

---
+++ 01_main.java
@@ -0,0 +1,12 @@
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.print("hello world with java");
+
+    }
+
+    
+
+    
+}
