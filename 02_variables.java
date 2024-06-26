e0c117ee6735250fba43acb4ae2945734786463e
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-26 20:46:10 +0530

    Create 02_variables.java

---
+++ 02_variables.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int age;
+        age=27;
+        int marks;
+         marks= 100;
+        System.out.println("i am "+ age + " years old");
+
+        System.out.println("my marks is "+ marks);
+    }
+    
+}
