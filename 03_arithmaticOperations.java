ce5f86ddb415364836cc607f847d5da3bb5d7897
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-28 13:25:12 +0530

    Create 03_arithmaticOperations.java

---
+++ 03_arithmaticOperations.java
@@ -0,0 +1,20 @@
+class Main{
+    public static void main(String[] args){
+        int number1 = 12;
+        int number2 = 6;
+        // addition
+        System.out.println(number1 + number2);
+        // difference
+        System.out.println(number1 - number2);
+        //division
+        System.out.println(number1 / number2);
+        // multiplication
+        System.out.println(number1 * number2);
+        // remainder
+        System.out.println(number1 % number2);
+
+        
+        
+    }
+    
+}
