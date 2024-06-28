8052bd92b87c83313b2558c91f8f0ac9e6dd722b
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-28 16:25:40 +0530

    Create 05_logicalOperators.java

---
+++ 05_logicalOperators.java
@@ -0,0 +1,16 @@
+class Main{
+    public static void main(String[] args){
+        int age =45;
+        // and
+        System.out.println(age>=18&& age<=40);
+        // or
+        
+        boolean isStudent= false;
+        boolean isLibraryMember= true;
+        System.out.println(isStudent || isLibraryMember);
+        
+        // not
+        System.out.println(!isStudent);
+    }
+    
+}
