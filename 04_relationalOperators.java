904d0eb5d57c170e9ee6b81c6fcce0615d030e14
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-28 16:15:05 +0530

    Create 04_relationalOperators.java

---
+++ 04_relationalOperators.java
@@ -0,0 +1,17 @@
+class Main{
+    public static void main(String[] args){
+        double  number1 = 12;
+        double number2 = 5;
+        System.out.println(number1==number2); 
+        // false
+        System.out.println(number1!=number2);
+        // true
+        System.out.println(number1>number2);
+        // true
+        System.out.println(number1<number2);
+        // false
+
+        
+    }
+    
+}
