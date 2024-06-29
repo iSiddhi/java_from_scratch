68797f9051edcfb3d4a7de6109f2cd77937b34d5
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-29 21:16:03 +0530

    Create 08_NewString.java

---
+++ 08_NewString.java
@@ -0,0 +1,14 @@
+
+// new keywprd is use to create a object from class
+class HelloWorld{
+    public static void main(String[] args){
+        // String name= "siddhi is my name ";
+        String name = new String("siddhi is my name");
+        System.out.println(name);
+        
+        
+    }
+}
+
+
+
