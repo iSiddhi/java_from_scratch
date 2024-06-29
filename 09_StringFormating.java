412e1a1df259e1b3ed401ca55875f66fe0770465
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-06-29 22:25:01 +0530

    Create 09_StringFormating.java

---
+++ 09_StringFormating.java
@@ -0,0 +1,21 @@
+
+// new keywprd is use to create a object from class
+class HelloWorld{
+    public static void main(String[] args){
+        String literalString1= "abc";
+        String literalString2="abc";
+        String objectString1= new String("xyz");
+        String objectString2= new String("xyz");
+
+        
+        System.out.println(literalString1==literalString2);
+        System.out.println(objectString1==objectString2);
+
+       
+        // System.out.println(name);
+        
+    }
+}
+
+
+
