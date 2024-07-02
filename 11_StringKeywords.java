6595dff352b5e77d45b1e6e39d4c3c14667a5f1d
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-02 23:16:45 +0530

    Create 11_StringKeywords.java

---
+++ 11_StringKeywords.java
@@ -0,0 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String name = " siddhi patel";
+        
+        
+        
+        // check the length of string
+        System.out.println(name.length());
+        //check wheather the string is empty or not 
+        // output will be boolean true or false 
+        System.out.println(name.isEmpty());
+        // convert into upercase
+        System.out.println(name.toUpperCase());
+        // convert to lowercase
+        System.out.println(name.toLowerCase());
+        
+    }
+}
