f1133223d0054204c4b4f71f9be40c0fe2baf828
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-02 23:28:26 +0530

    Create 13_StringKeywords.java

---
+++ 13_StringKeywords.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String string= "sky is blue";
+        System.out.println(string.replace("blue","red"));
+        System.out.println(string.contains("sky"));
+    }
+}
