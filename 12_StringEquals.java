4c1434e2eb28e459218db3905f0c23fae49162de
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-02 23:22:11 +0530

    Create 12_StringEquals.java

---
+++ 12_StringEquals.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+    String String1= new String("abc");
+    String String2 = new String("ABC");
+    System.out.println(String1.equals(String2));
+    //  ignore cases and find if strings are equal of not 
+    System.out.println(String1.equalsIgnoreCase(String2));
+
+        
+        
+    }
+}
