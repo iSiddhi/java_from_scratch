856fe7fa22b95d76df63e5e51ac2834340130aed
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-04 22:10:08 +0530

    Create 16_ForamtSpecifier.java

---
+++ 16_ForamtSpecifier.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+
+class HelloWorld{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what is your name ");
+        
+        // this is used to take string as input from the user;
+        String name = scanner.nextLine();
+        
+        System.out.printf("hello %s . how are you ??",name);
+        scanner.close();
+        
+    }
+}
