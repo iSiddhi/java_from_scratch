e89f615ddede7d742064686b6e14dd2160807746
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-04 21:59:48 +0530

    Create 15_StringInput.java

---
+++ 15_StringInput.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+
+class HelloWorld{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        // this is used to take string as input from the user;
+        String name = scanner.nextLine();
+        System.out.println(name);
+        scanner.close();
+        
+    }
+}
