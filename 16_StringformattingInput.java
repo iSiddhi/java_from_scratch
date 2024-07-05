6f021b5bb3b92bada4fc78f699b5f48fd3ab779b
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-05 22:22:41 +0530

    Create 16_StringformattingInput.java

---
+++ 16_StringformattingInput.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what is you name");
+        String name = scanner.nextLine();
+        
+        System.out.println("heyy my name is %s");
+        int age = scanner.nextInt();
+        System.out.printf("%d is an excellent age to start programming.What language do you prefer ?",age);
+        String language= scanner.nextLine();
+        System.out.println("%s is very popular language",language);
+        scanner.close();
+        
+        // System.out.println("Try programiz.pro");
+    }
+}
