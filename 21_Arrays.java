a34a278b36684a0b0a19782a75e1011a6958ca85
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-09 22:37:37 +0530

    Create 21_Arrays.java

---
+++ 21_Arrays.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Main{
+    public static void main(String[] args){
+        // store a roll number 
+        int a = 19;
+        // store a persons name
+        String name = "siddhi";
+        // store 5 roll number 
+        // array is collection of datatypes
+        // SYNTAX
+        // datatype[] variable_name= new datatype[size]
+        int[] rollno= new int[5];
+        // or 
+        int[] rollnos= {23,12,45,32,15};
+        ""
+        int [] rolls ;
+        // 'declaration of array, rolls is getting defined in a stack '
+        ros= new int[5];
+        // initialization, actually here object is being created in the memory(heap)
+        
+        
+        
+        
+    }
+}
