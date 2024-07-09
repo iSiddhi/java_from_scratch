b3ff4531de49c87fe5666d975158ac625b7f5c3c
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-09 23:17:00 +0530

    Create 22_array.java

---
+++ 22_array.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Main{
+    public static void main(String[] args){
+       int[] arr= new int[5];
+       arr[0]=23;
+       arr[1]=45;
+       arr[2]=45;
+       arr[3]=45;
+       arr[4]=678;
+       System.out.println(arr[3]);
+       
+       
+    //   time consuming so we will use array inputs
+        
+        
+        
+        
+        
+        
+    }
+}
