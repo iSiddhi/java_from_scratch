b921fec2ecd6506fe00ceb9724a463a9b37825ef
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-07-14 23:16:35 +0530

    Create 30_array.java

---
+++ 30_array.java
@@ -0,0 +1,51 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+
+//[multidimensional ]
+//craete an array that hve matrix like this
+//123
+//456
+//789
+int [][] arr= new int[3][3];
+//first is number of rows
+//second is number  of colums
+//number of colums is not necessary to write 
+//adding rows is madatory
+int[][] arr= {
+  {1,2,3};
+    {4,5,6};
+    {7,8,9};
+
+
+
+
+
+
+
+
+
+};
+
+//its continuos memory allocation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+ 
+}
